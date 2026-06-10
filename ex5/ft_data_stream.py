3dc2bc89292be0b016a650c7304b7405e3bc39f4
todina-r <ratsinjomananatolotra@gmail.com>
2026-06-10 19:25:52 +0300

    review ex5

--- ex5/ft_data_stream.py
+++ ex5/ft_data_stream.py
@@ -3,14 +3,11 @@ import random
 
 
 def gen_event(n: int) -> Generator[tuple[str, str], None, None]:
-    name_list: list[str]
-    action_list: list[str]
-
-    action_list = [
+    action_list: list[str] = [
         "run", "eat", "sleep", "grab",
         "move", "climb", "swim", "release",
     ]
-    name_list = [
+    name_list: list[str] = [
         "alice", "bob", "charlie", "dylan",
     ]
     for i in range(n):
@@ -21,10 +18,8 @@ def gen_event(n: int) -> Generator[tuple[str, str], None, None]:
 def consume_event(
     event_list: list[tuple[str, str]]
 ) -> Generator[tuple[str, str], None, None]:
+    initial_len: int = len(event_list)
     rand_index: int
-    initial_len: int
-
-    initial_len = len(event_list)
     for i in range(len(event_list)):
         rand_index = random.randrange(
             0, initial_len - i
@@ -34,16 +29,15 @@ def consume_event(
 
 if __name__ == "__main__":
     print("=== Game Data Stream Processor ===")
-    event_count: int
-    event_list: list[tuple[str, str]]
-
-    event_count = 0
+    event_count: int = 0
     for (name, action) in gen_event(1000):
         print(f"Event: {event_count}:",
               f"Player {name} did",
-              f"action {action}")
+              f"actio {action}")
         event_count += 1
-    event_list = [event for event in gen_event(10)]
+    event_list: list[tuple[str, str]] = [
+        event for event in gen_event(10)
+    ]
     print("Built list of 10 events:", event_list)
     for event in consume_event(event_list):
         print("Got event from list:", event)
