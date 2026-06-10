32948cb91fda620f196e7b8292cb41530605f1a8
todina-r <ratsinjomananatolotra@gmail.com>
2026-06-10 19:29:10 +0300

    review ex6

--- ex6/ft_data_alchemist.py
+++ ex6/ft_data_alchemist.py
@@ -2,30 +2,27 @@ import random
 
 
 if __name__ == "__main__":
-    mixed_list: list[str]
-    filtered_list: list[str]
-    transformed_list: list[str]
-    score: dict[str, int]
-    avg_score: float
-    high_score: dict[str, int]
-
-    mixed_list = [
+    mixed_list: list[str] = [
         "Alice", "bob", "Charlie",
         "dylan", "Emma", "Gregory",
         "john", "kevin", "Liam"
     ]
-    filtered_list = [
+    filtered_list: list[str] = [
         name for name in mixed_list if name[0].isupper()
     ]
-    transformed_list = [
+    transformed_list: list[str] = [
         name.capitalize() for name in mixed_list
     ]
     print("Initial list of players:", mixed_list)
     print("New list with all names capitalized:", transformed_list)
     print("New list of capitalized names only:", filtered_list)
-    score = {name: random.randint(0, 1000) for name in transformed_list}
-    avg_score = round(sum(score.values()) / len(score), 2)
-    high_score = {key: val for key, val in score.items() if val > avg_score}
+    score: dict[str, int] = {
+        name: random.randint(0, 1000) for name in transformed_list
+    }
+    avg_score: float = round(sum(score.values()) / len(score), 2)
+    high_score: dict[str, int] = {
+        key: val for key, val in score.items() if val > avg_score
+    }
     print("Score dict:", score)
     print("Score average is", avg_score)
     print("High scores:", high_score)
