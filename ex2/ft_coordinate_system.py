049f8decad32c189426fee3a54e6b3769e5bedd3
todina-r <ratsinjomananatolotra@gmail.com>
2026-06-10 19:03:32 +0300

    review ex2

--- ex2/ft_coordinate_system.py
+++ ex2/ft_coordinate_system.py
@@ -1,34 +1,52 @@
 import math
 
 
-def get_coord_input() -> list[str]:
-    line: str = input("Enter new coordinates " +
-                      "as floats in format " +
-                      "'x,y,z': ")
-    return line.split(",")
-
-
-def convert_list_content(lst: list[str]) -> list[float]:
-    res: list[float] = []
-    for elem in lst:
-        try:
-            res.append(float(elem))
-        except ValueError as error:
-            print("Error on parameter",
-                  f"'{elem}': {error}")
-            res.clear()
+def get_coord_input() -> tuple[str, str, str] | None:
+    res: tuple[str, str, str] | None = None
+    input_list: list[str] | None = None
+    input_prompt: str = (
+        "Enter new coordinates "
+        "as floats in format "
+        "'x,y,z': "
+    )
+    try:
+        line: str = input(input_prompt)
+        input_list: list[str] = line.split(",")
+    except BaseException:
+        pass
+    if input_list and len(input_list) == 3:
+        res = (input_list[0], input_list[1], input_list[2])
     return res
 
 
-def get_player_pos() -> tuple[float, float, float]:
-    coord: tuple[float, float, float] = ()
+def try_convert_str_to_float(s: str) -> float:
+    try:
+        return float(s)
+    except ValueError as error:
+        raise ValueError(
+            f"Error on parameter '{s}': "
+            + str(error)
+        )
+
+
+def convert_list_content(coord_str: tuple[str, str, str]) -> tuple[float]:
+    try:
+        return (
+            try_convert_str_to_float(coord_str[0]),
+            try_convert_str_to_float(coord_str[1]),
+            try_convert_str_to_float(coord_str[2])
+        )
+    except ValueError as error:
+        print(error)
+
+
+def get_player_pos() -> tuple[float, float, float] | None:
+    coord: tuple[float, float, float] | None = None
     coord_str: list[str]
     while not coord:
         coord_str = get_coord_input()
-        if len(coord_str) == 3:
-            coord = tuple[float](
-                convert_list_content(coord_str)
-            )
+        if coord_str:
+            coord = convert_list_content(coord_str)
         else:
             print("Invalid syntax")
     return coord
@@ -63,4 +81,4 @@ if __name__ == "__main__":
     print("\nGet a second set of coordinates")
     coord2 = get_player_pos()
     print("Distance between the 2 sets of coordinates:",
-          f"{get_dist(coord1, coord2):.4f}")
+          f"{round(get_dist(coord1, coord2), 4)}")
